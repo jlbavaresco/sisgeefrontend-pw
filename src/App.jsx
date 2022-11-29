d91cb646f8971b884f0c8c9cadf6104f7e19ef66
Jorge Bavaresco <jlbavaresco@gmail.com>
2022-11-29 10:53:45 -0300

    Commit inicial do projeto

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import '@popperjs/core/dist/cjs/popper.js'
+
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+import Menu from './componentes/Menu'
+
+import Home from './componentes/Home'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Predio from './componentes/telas/predio/Predio'
+
+
+function App() {
+
+  return (
+
+    <Router>
+
+      <Menu />
+
+      <Routes>
+
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/predios" element={<Predio/>}/>
+
+      </Routes>
+
+    </Router>
+
+  );
+
+}
+
+export default App;
